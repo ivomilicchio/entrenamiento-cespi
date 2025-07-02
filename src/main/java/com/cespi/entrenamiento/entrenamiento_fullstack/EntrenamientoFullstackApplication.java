c2218ce7f907bd57ac5dbb5497e5f8485bcc30ec
Ivo Milicchio <ivomilicchio123@gmail.com>
2025-07-02 13:26:02 -0300

    Subo proyecto con configuración incial

---
+++ src/main/java/com/cespi/entrenamiento/entrenamiento_fullstack/EntrenamientoFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.cespi.entrenamiento.entrenamiento_fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  EntrenamientoFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrenamientoFullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cespi/entrenamiento/entrenamiento_fullstack/EntrenamientoFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cespi.entrenamiento.entrenamiento_fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrenamientoFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
